4e2abdee8cb4773c3a69cd930b7bd79460058ca3
JeHoon <JeHoon@DESKTOP-0NU9ABL>
2020-11-10 18:44:25 +0900

    메소드명 수정

--- src/main/java/com/wMember/login/LoginRouter.java
+++ src/main/java/com/wMember/login/LoginRouter.java
@@ -27,12 +27,12 @@ public class LoginRouter {
 	}
 	
 	/**
-	 * api router
+	 * loginRouter
 	 * @param handler
 	 * @return
 	 */
 	@Bean
-	public RouterFunction<ServerResponse> loginRouter(LoginHandler loginHandler){
+	public RouterFunction<ServerResponse> sessionRouter(LoginHandler loginHandler){
 		return RouterFunctions			
 			//로그인 프로세스
 			.route(RequestPredicates.POST("/api/member/loginProcess")
